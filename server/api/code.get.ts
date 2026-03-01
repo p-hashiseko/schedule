b6630f17e1f2a0ea0bec75f1c0aeea0c26df17f7
p-hashiseko <program.reiji@gmail.com>
2026-03-02 00:57:25 +0900

    fix: linter formatter

--- server/api/code.get.ts
+++ server/api/code.get.ts
@@ -1,10 +1,10 @@
 // server/api/code.get.ts
-export default defineEventHandler((event) => {
+export default defineEventHandler(() => {
   // コンソールにログを出して、リクエストが届いたか確認しやすくします
-  console.log('Backend: Request received!');
-  
+  console.log('Backend: Request received!')
+
   // 指定されたメッセージを返します
   return {
-    message: "バックエンドと接続完了です！"
-  };
-});
+    message: 'こんにちは、こちらはバックエンドです'
+  }
+})
